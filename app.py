e4eff10d0ec100cd4a22033dcbaf38e40058c307
Jorge Q <43147704+AHINTOR@users.noreply.github.com>
2024-09-20 23:40:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ pg = st.navigation(
 # --- SHARED ON ALL PAGES ---
 st.logo("images/starling_rap.svg")
 st.sidebar.markdown("Hecho con **python y streamlit** en el curso de _Cuadernos de código Python_ ❤️ by [Ahintor](https://anestesia.wiki)")
-
+st.divider()  # 👈 Draws a horizontal rule
 
 # --- RUN NAVIGATION ---
 pg.run()
