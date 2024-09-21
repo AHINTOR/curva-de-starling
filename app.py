dfbed063df565c9e08106c50afc92e2d92197f85
Jorge Q <43147704+AHINTOR@users.noreply.github.com>
2024-09-20 23:01:27 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,48 @@
+import streamlit as st
+import patient_equations as eq
+import matplotlib.pyplot as plt
+
+# Título de la aplicación
+st.title("Curva de Frank Starling")
+st.markdown("_Adaptado por ***Jorge Quispe***:_")
+#st.write("Adaptado por Jorge Quispe")
+#st.write("Ingresa las siguientes variables:")
+
+# --- PAGE SETUP ---
+about_page = st.Page(
+    "pages/formula.py",
+    title="Curva Frank Starling",
+    icon=":material/monitoring:",
+    default=True,
+)
+project_1_page = st.Page(
+    "pages/problema.py",
+    title="Problema",
+    icon=":material/problem:",
+)
+project_2_page = st.Page(
+    "pages/referencias.py",
+    title="Referencias",
+    icon=":material/developer_guide:",
+)
+
+
+# --- NAVIGATION SETUP [WITHOUT SECTIONS] ---
+# pg = st.navigation(pages=[about_page, project_1_page, project_2_page])
+
+# --- NAVIGATION SETUP [WITH SECTIONS]---
+pg = st.navigation(
+    {
+        "App": [about_page],
+        "Documentación": [project_1_page, project_2_page],
+    }
+)
+
+
+# --- SHARED ON ALL PAGES ---
+#st.logo("images/FS.jpeg")
+#st.sidebar.markdown("Made with ❤️ by [Sven](https://youtube.com/@codingisfun)")
+
+
+# --- RUN NAVIGATION ---
+pg.run()
