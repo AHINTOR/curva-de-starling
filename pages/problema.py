941f59e1b74885f9a1d74e9122360a1734c5bf59
Jorge Q <43147704+AHINTOR@users.noreply.github.com>
2024-09-21 00:51:20 -0500

    Update problema.py

--- pages/problema.py
+++ pages/problema.py
@@ -14,8 +14,11 @@ with col2:
 st.markdown("## Justificación de la aplicación:")
 st.markdown("Esta aplicación busca proporcionar una alternativa no invasiva para calcular estos parámetros y ubicarlos en la curva de Frank-Starling, usando métodos indirectos como el cálculo del CO por el método de Fick modificado y la estimación de LVEDP con la ecuación publicada por Abd-El-Aziz. De esta manera, los clínicos podrán obtener una visión aproximada del estado hemodinámico del paciente sin necesidad de procedimientos invasivos, lo que puede reducir complicaciones y permitir una respuesta más rápida en el manejo clínico.")
 st.markdown("## Aplicación:")
-st.markdown("La aplicación está diseñada para calcular de forma automática los parámetros clave y graficar la posición del paciente en la curva de Frank-Starling, facilitando la interpretación rápida por parte de los médicos. Con estos cálculos, se puede ajustar el tratamiento del paciente con más precisión, ayudando a decidir si es necesario administrar líquidos o apoyo inotrópico. El uso de variables como la edad, la presión arterial, la saturación de oxígeno y la fracción de eyección hace que el proceso sea accesible con datos clínicos de rutina, permitiendo una mayor aplicabilidad en entornos médicos con recursos limitados.")
+st.markdown("La aplicación está diseñada para calcular de forma automática los parámetros clave y graficar la posición del paciente en la curva de Frank-Starling, facilitando la interpretación rápida por parte de los médicos. Con estos cálculos, se puede ajustar el tratamiento del paciente con más precisión, ayudando a decidir si es necesario administrar líquidos o apoyo inotrópico. ")
+st.markdown("La fórmula para determinar el LVEDP usada para es app es la siguiente")
+st.latex("LVEDP = [0.54 \times MABP \times (1 - EF)] - 2.23")
+st.markdown("Para el cálculo del gasto cardiaco se usa el método de Fick, donde la VO2 depende de la edad y la superficie corporal.")
+st.latex("GC = VO2 \ C(A - V)O2")
 st.markdown("De esta forma, la aplicación tiene un gran potencial para mejorar la atención de pacientes críticos, optimizando los recursos disponibles y reduciendo el riesgo asociado a procedimientos invasivos.")
-st.latex("\LVEDP = [0.54 \times MABP \times (1 - EF)] - 2.23")
 
 st.image("images/starling.png", caption="resultado de la app")
