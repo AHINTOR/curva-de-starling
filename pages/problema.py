7f225c385f91ca6d37aea04a6c70b4f3d5b89d24
Jorge Q <43147704+AHINTOR@users.noreply.github.com>
2024-09-21 00:56:33 -0500

    Update problema.py

--- pages/problema.py
+++ pages/problema.py
@@ -16,9 +16,9 @@ st.markdown("Esta aplicación busca proporcionar una alternativa no invasiva par
 st.markdown("## Aplicación:")
 st.markdown("La aplicación está diseñada para calcular de forma automática los parámetros clave y graficar la posición del paciente en la curva de Frank-Starling, facilitando la interpretación rápida por parte de los médicos. Con estos cálculos, se puede ajustar el tratamiento del paciente con más precisión, ayudando a decidir si es necesario administrar líquidos o apoyo inotrópico. ")
 st.markdown("La fórmula para determinar el LVEDP usada para es app es la siguiente")
-st.latex("LVEDP = [0.54\frac MABP\frac (1 - EF)] - 2.23")
+st.latex("LVEDP = [0.54\fracMABP\frac(1-EF)]-2.23")
 st.markdown("Para el cálculo del gasto cardiaco se usa el método de Fick, donde la VO2 depende de la edad y la superficie corporal.")
-st.latex("GC =\frac{VO_2}{C(a - v)O_2}")
+st.latex("GC =\frac{VO_2}{C(a-v)O_2}")
 st.latex(r'''
     a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
     \sum_{k=0}^{n-1} ar^k =
