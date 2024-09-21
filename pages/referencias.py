2fbb1dd01aec94cd397dc80f20a97c4a79dc4f0a
Jorge Q <43147704+AHINTOR@users.noreply.github.com>
2024-09-21 00:36:38 -0500

    Update referencias.py

--- pages/referencias.py
+++ pages/referencias.py
@@ -2,4 +2,9 @@ import streamlit as st
 import patient_equations as eq
 import matplotlib.pyplot as plt
 
-st.markdown(":arrow_forward:[https://github.com/AHINTOR/curva-de-starling](https://github.com/AHINTOR/curva-de-starling/tree/main)")
+st.subheader("Link al repositorio Github de este proyecto:")
+st.markdown(":arrow_forward: [https://github.com/AHINTOR/curva-de-starling](https://github.com/AHINTOR/curva-de-starling/tree/main)")
+st.subheader("Link al repositorio Github original")
+st.markdown(":arrow_forward: [https://github.com/dzauski585/Starling_Curve](https://github.com/dzauski585/Starling_Curve)")
+st.subheader("Bibliografía recomendada:")
+st.markdown("Abd-El-Aziz T. A. (2012). Noninvasive prediction of left ventricular end-diastolic pressure in patients with coronary artery disease and preserved ejection fraction. The Canadian journal of cardiology, 28(1), 80–86. [https://doi.org/10.1016/j.cjca.2011.02.001](https://doi.org/10.1016/j.cjca.2011.02.001)")
